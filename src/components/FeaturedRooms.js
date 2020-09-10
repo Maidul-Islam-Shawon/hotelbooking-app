c4cd7aa4fcb810256a071e1ccd55b0e1f3454f68
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2020-09-10 17:50:07 +0100

    featuredrooms and context updated.

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,8 +4,8 @@ import { RoomContext } from "../Context";
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const value = this.context;
-    console.log(value);
-    return <div>This is FeaturedRooms---{value}</div>;
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
+    return <div>This is FeaturedRooms</div>;
   }
 }
