01a97d3f9e3042631cac53c28b10bb54a4bffda6
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2020-09-11 17:49:25 +0100

    featured rooms are ready.

--- src/Context.js
+++ src/Context.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import items from "./data";
 
+//........video starts from 2:39:44..........//
+
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
@@ -34,9 +36,15 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom(slug) {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  }
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
