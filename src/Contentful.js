9772874e2ca2a52350b3302f43ef918cd06c6974
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2020-09-24 14:30:05 +0100

    update git ignore and env

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "iayjm6829l68",
-  accessToken: "dffcJUNmgEMyncbHDs7TmW9klK9cS3ljqdujfJHo2gQ",
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
